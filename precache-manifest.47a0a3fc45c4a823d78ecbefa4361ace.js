6bc89c0f5f23e68ec17ebece033585fc596ed242
Trusha Sakharkar <trushasakharkar@gmail.com>
2020-05-05 18:40:42 +0530

    Updates

---
+++ precache-manifest.47a0a3fc45c4a823d78ecbefa4361ace.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2c2714752fe0580b1666e6d7de30a2",
+    "url": "/React-with-hooks_shoppls/index.html"
+  },
+  {
+    "revision": "cb079e0a41b8db347df0",
+    "url": "/React-with-hooks_shoppls/static/css/2.06798d3e.chunk.css"
+  },
+  {
+    "revision": "21709856bf1579716733",
+    "url": "/React-with-hooks_shoppls/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cb079e0a41b8db347df0",
+    "url": "/React-with-hooks_shoppls/static/js/2.b6d0a39e.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/React-with-hooks_shoppls/static/js/2.b6d0a39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21709856bf1579716733",
+    "url": "/React-with-hooks_shoppls/static/js/main.6b8c58be.chunk.js"
+  },
+  {
+    "revision": "9ae0e466f373085363c5",
+    "url": "/React-with-hooks_shoppls/static/js/runtime-main.17054fcc.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/React-with-hooks_shoppls/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/React-with-hooks_shoppls/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/React-with-hooks_shoppls/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/React-with-hooks_shoppls/static/media/notification.c0d3c94c.eot"
+  }
+]);
